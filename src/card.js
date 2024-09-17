f40f6e5435959e137c33af8b9b28d13c3002cd94
Chisom Ukeh <139554931+chisomukeh@users.noreply.github.com>
2024-09-17 14:41:07 +0100

    Update card.js

--- src/card.js
+++ src/card.js
@@ -1,8 +1,16 @@
 //Card Object 
-function Card(shape, figure ){
+function Card(shape, figure){
   this.shape = shape;
-  this .figure = figure;
+  this.figure = figure;
 }
+
+
+
+Card.prototype.size = function(){
+  if(this.shape == "star") return this.figure * 2;
+  return  this.figure;
+};
+
 
 
 module.exports = Card;
