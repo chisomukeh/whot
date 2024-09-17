c37e3040f986d237c881b15fa1f2b125522e91df
Chisom Ukeh <139554931+chisomukeh@users.noreply.github.com>
2024-09-17 12:43:53 +0100

    Update whot.js

--- whot.js
+++ whot.js
@@ -1,1 +1,13 @@
+const Card = require("./src/card.js");
+const shuffle = require("./src/shuffle.js");
+const loadCards = require("./src/loadcards.js");
+const whot = require("./src/whot.js");
 
+
+
+module.exports = {
+  Card,
+  shuffle,
+  loadCards,
+  whot
+};
